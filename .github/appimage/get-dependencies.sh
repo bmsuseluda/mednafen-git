06895b424a83c02b0db380ff4bc74e1f4f64bd07
Dennis Ludwig <dennis.ludwig@aoe.com>
2026-09-24 12:24:56 +0200

    add github action to create appimage

---
+++ .github/appimage/get-dependencies.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	cmake     \
+	gcc-libs  \
+	libao     \
+	libx11    \
+	libxrandr \
+	libxss    \
+	openal    \
+	pkgconf   \
+	sdl2      \
+	zlib      \
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
+
+(
+	./configure --prefix="/usr"
+	make -j"$(nproc)"
+	make install
+	make installcheck
+	make clean
+	make distclean
+)
+
